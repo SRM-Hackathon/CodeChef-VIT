026e5aeaf291354f36593e903dc5e8a89e9565d2
Dipto Chakrabarty <diptochuck123@gmail.com>
2019-10-11 20:47:51 +0530

    updated

--- S3add_trust.py
+++ S3add_trust.py
@@ -1,19 +1,28 @@
-# How to add users in the trusted S3 bucket
 import csv
 import boto3
+#import pandas as pd
 
-with open('AWS creds.csv','r') as input:
+trusted= "dipto"
+with open('AWScreds.csv','r') as input:
     next(input)
     reader= csv.reader(input)
     for line in reader:
         access_key_id = line[2]
         secret_access_key = line[3]
 # take user name to add
-name=input("Enter the user name")
-photo= './images/{}.jpg'.format(name)
+#name=input("Enter the user name")
+faces=[]
+photo= './images/src.png'
+# Taking photo directory
+#name= "dipto"
+faces.append(trusted)
+print(trusted)
 # Save Image with users name
-faces.append(name)
+#faces.append(name)
 s3= boto3.client('s3',
                     aws_access_key_id= access_key_id ,
                         aws_secret_access_key=secret_access_key, region_name='ap-south-1')
-s3.upload_file('./images/bus.jpg','trusted-members','{}.jpg').format(name)
+s3.upload_file('./images/src.png','trusted-members','{}.png'.format(trusted))
+f= open('trusted.txt','a+')
+f.write("{}\n".format(trusted))
+f.close()
--- face_compare.py
+++ face_compare.py
@@ -1,33 +1,39 @@
 import csv
 import boto3
-
-with open('AWS creds.csv','r') as input:
+#import S3add_trust as S3tr
+with open('AWScreds.csv','r') as input:
     next(input)
     reader= csv.reader(input)
     for line in reader:
         access_key_id = line[2]
         secret_access_key = line[3]
-source_photo= './images/src.jpg'
-target_photo= './images/tar.jpg'
+source_photo= './images/tar.png'
+#target_photo= './images/tar.jpg'
 client= boto3.client('rekognition',
                     aws_access_key_id= access_key_id ,
                         aws_secret_access_key=secret_access_key, region_name='ap-south-1')
 with open(source_photo, 'rb') as source_image:
     source_bytes = source_image.read()
-with open(target_photo, 'rb') as source_image:
-    target_bytes = source_image.read()
-
-response = client.compare_faces(SourceImage={
-        'Bytes': source_bytes},
-    TargetImage={
-        'Bytes': target_bytes}
-                )
+#with open(target_photo, 'rb') as source_image:
+#    target_bytes = source_image.read()
+f= open("trusted.txt","r")
+if f.mode== "r":
+    contents = f.read()
+    response = client.compare_faces(SourceImage={
+            'Bytes': source_bytes},
+            TargetImage={
+            'S3Object': {
+                    'Bucket': 'trusted-members',
+                    'Name': 'dipto.png'
+                    }
+                }
+                    )
 # Use Attributes parameter for details other than default values
 #print(response)
 #print(type(response))
 #print(response['Labels'][0]['Confidence'])
-for key,value in response.items():
-    if key in ('FaceMatches','UnmatchedFaces'):
-        print(key)
-        for att in value:
-            print(att)
+    for key,value in response.items():
+        if key in ('FaceMatches','UnmatchedFaces'):
+            print(key)
+            for att in value:
+                print(att)
