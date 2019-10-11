cb2a01ab090c469a61867a00705ee40cd83d2f1b
Dipto Chakrabarty <diptochuck123@gmail.com>
2019-10-11 18:12:09 +0530

    add bucket name

--- S3add_trust.py
+++ S3add_trust.py
@@ -16,4 +16,4 @@ faces.append(name)
 s3= boto3.client('s3',
                     aws_access_key_id= access_key_id ,
                         aws_secret_access_key=secret_access_key, region_name='ap-south-1')
-s3.upload_file('./images/bus.jpg','imagehack','bus.jpg')
+s3.upload_file('./images/bus.jpg','trusted-members','{}.jpg').format(name)
