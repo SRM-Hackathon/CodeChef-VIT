720bd963087418081ff2928299af39174c73fa42
Dipto Chakrabarty <diptochuck123@gmail.com>
2019-10-11 15:30:15 +0530

    src and tar

--- face_compare.py
+++ face_compare.py
@@ -7,3 +7,12 @@ with open('AWS creds.csv','r') as input:
     for line in reader:
         access_key_id = line[2]
         secret_access_key = line[3]
+source_photo= './images/src.jpg'
+target_photo= './images/tar.jpg'
+client= boto3.client('rekognition',
+                    aws_access_key_id= access_key_id ,
+                        aws_secret_access_key=secret_access_key, region_name='ap-south-1')
+with open(source_photo, 'rb') as source_image:
+    source_bytes = source_image.read()
+with open(target_photo, 'rb') as source_image:
+    target_bytes = source_image.read()
