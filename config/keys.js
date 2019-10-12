9a0f823eb7755bfaeb1d85508c9fc0fd9335d0f9
RajatSablok <rajat.main06@gmail.com>
2019-10-12 10:00:15 +0530

    added mvc and routes

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+dbPassword = 'mongodb+srv://RajatSablok:dpsbplsthbst@loginidpass-dd8no.mongodb.net/test?retryWrites=true&w=majority';
+
+// 'mongodb+srv://RajatSablok:dpsbplsthbst@loginidpass-dd8no.mongodb.net/test?retryWrites=true&w=majority'
+
+module.exports = {
+    mongoURI: dbPassword
+};
