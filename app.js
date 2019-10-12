2743df0965ce993e25fdea757de77794d00f5611
RajatSablok <rajat.main06@gmail.com>
2019-10-12 12:39:04 +0530

    better comments2

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ mongoose
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-// EJS
+// Set template to EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
