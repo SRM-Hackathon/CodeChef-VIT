fb04e35cbc0c69a51caf7e6d14e172aeff1176aa
Dipto Chakrabarty <45638240+DiptoChakrabarty@users.noreply.github.com>
2019-10-12 10:02:29 +0530

    Delete keys.js

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-dbPassword = 'mongodb+srv://RajatSablok:dpsbplsthbst@loginidpass-dd8no.mongodb.net/test?retryWrites=true&w=majority';
-
-// 'mongodb+srv://RajatSablok:dpsbplsthbst@loginidpass-dd8no.mongodb.net/test?retryWrites=true&w=majority'
-
-module.exports = {
-    mongoURI: dbPassword
-};
